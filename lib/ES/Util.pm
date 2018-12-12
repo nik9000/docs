171ac0c4255ff1ce3c89d67dc7d40f6de9f91ab1
Nik Everett <nik9000@gmail.com>
2018-12-12 16:53:20 -0500

    Switch back to just returning stdout
    This fixes commands that need the result of stdout.

--- lib/ES/Util.pm
+++ lib/ES/Util.pm
@@ -459,13 +459,13 @@ sub run (@) {
         ( $out, $err, $ok ) = capture { system(@args) == 0 };
     }
 
-    return "$out\n$err" if $ok;
+    return $out if $ok;
 
     my $git_dir = $ENV{GIT_DIR} ? "in GIT_DIR $ENV{GIT_DIR}" : "";
     die "Error executing: @args $git_dir\n$out\n---\n$err"
         unless $ok;
 
-    return "$out\n$err";
+    return $out;
 }
 
 #===================================
